14f121c11963c0d011d769a346b5ec0544c7114c
Art Tucker <art.w.tucker@gmail.com>
2021-02-08 23:52:55 -0800

    Update app.js

---
+++ static/js/app.js
@@ -0,0 +1,26 @@
+// import data from data.js
+const tableData = data;
+
+// reference the HTML table using d3
+var tbody = d3.select("tbody");
+
+function buildTable(data) {
+    // Clear any existing data
+    tbody.html("");
+
+    // Loop through each object in data and
+    // append a row and cells for each value in row
+    data.forEach((dataRow) => {
+        // Append row to table body
+        let row = tbody.append("tr");
+
+        // Loop through each field in dataRow and
+        // add each value as a table cell (td)
+        Object.values(dataRow).forEach((val) => {
+            let cell = row.append("td");
+            cell.text(val);
+            }
+        );
+    });
+}
+
